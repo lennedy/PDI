f2409a7a4a63e783cf1da45c62154fc3094eac10
lennedy <lennedy@gmail.com>
2017-05-21 10:34:37 -0300

    commit inicial

---
+++ hough/hough.cpp
@@ -0,0 +1,48 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void help()
+{
+ cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+         "Usage:\n"
+         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
+}
+
+int main(int argc, char** argv)
+{
+ const char* filename = argc >= 2 ? argv[1] : "pic4.jpg";
+
+ Mat src = imread(filename, 0);
+ if(src.empty())
+ {
+     help();
+     cout << "can not open " << filename << endl;
+     return -1;
+ }
+
+ Mat dst, cdst;
+ Canny(src, dst, 50, 200, 3);
+ cvtColor(dst, cdst, CV_GRAY2BGR);
+
+  vector<Vec4i> lines;
+  HoughLinesP(dst, lines, 1, CV_PI/180, 35, 1, 50 );
+  for( size_t i = 0; i < lines.size(); i++ )
+  {
+    Vec4i l = lines[i];
+    line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+  }
+
+ imshow("source", src);
+ imshow("detected lines", cdst);
+
+	cout << lines.size() << endl;
+
+ waitKey();
+
+ return 0;
+}
